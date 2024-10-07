f916e380d053755d84519163a1bcd7abb0b2e78d
Zachary Lilley <zachary.d.lilley-1@ou.edu>
2024-10-07 12:54:17 -0500

    Authentication/fication Compartmentalization 10/7/24

--- functions/index.js
+++ functions/index.js
@@ -33,10 +33,3 @@ exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
         });
     });
 });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
