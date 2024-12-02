6b4976ec90c0cbfa9c04a1124dcf484ef499b5e8
TubAleeLo <tub-a-lo@outlook.com>
2024-12-02 16:34:23 -0600

    edit

--- src/auth.js
+++ src/auth.js
@@ -2,39 +2,16 @@
 let auth, db, storage;
 
 // Declare firebaseConfigUrl
-let firebaseConfigUrl;
-const useEmulator = false;
+const firebaseConfigUrl = 'https://us-central1-projectw-6c4cd.cloudfunctions.net/getFirebaseConfig';
 
-// For local Development on the emulator
-if (true) {
-    fetch(firebaseConfigUrl).then(response => response.json()).then(config => {
+// Fetch Firebase configuration and initialize Firebase services
+fetch(firebaseConfigUrl)
+    .then(response => response.json())
+    .then(config => {
+        // Initialize Firebase app with the fetched configuration
         firebase.initializeApp(config);
 
-        auth = firebase.auth();
-        db = firebase.firestore();
-        storage = firebase.storage();
-
-        if (useEmulator) {
-            auth.useEmulator("http://localhost:9099");
-            db.useEmulator("http://localhost:8099");
-            storage.useEmulator("http://localhost:9199");
-            console.log('Firebase emulators initialized');
-        }
-
-        auth.onAuthStateChanged(user => {
-            if (user != null) {
-                console.log("Auth State Changed: " + user.email);
-            }
-        });
-    }).catch(error => {
-        console.error("Error fetching Firebase config:", error.message);
-        console.error("Stack trace:", error.stack);
-    });
-} else {
-    firebaseConfigUrl = 'https://us-central1-projectw-6c4cd.cloudfunctions.net/getFirebaseConfig';
-    fetch(firebaseConfigUrl).then(response => response.json()).then(config => {
-        firebase.initializeApp(config);
-
+        // Initialize Firebase services
         auth = firebase.auth();
         db = firebase.firestore();
         storage = firebase.storage();
@@ -45,7 +22,8 @@ if (true) {
                 console.log("Auth State Changed: " + user.email);
             }
         });
-    }).catch(error => {
-        console.error("Error fetching Firebase config:", error);
+    })
+    .catch(error => {
+        console.error("Error fetching Firebase config:", error.message);
+        console.error("Stack trace:", error.stack);
     });
-}
