7cfcec53479b7ff1c636fa647b924b08595e6c4a
Thorpe Mayes <tmayesiv@gmail.com>
2024-12-06 10:41:57 -0600

    accepted changes

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,6 @@ try {
     const STORAGE_BUCKET = functions.config().sec.storage_bucket;
     const APP_ID = functions.config().sec.app_id;
 
-<<<<<<< HEAD
     // Define allowed origins
     const allowedOrigins = ['http://127.0.0.1:5500', 'http://localhost:5001', 'http://localhost:3000', 'http://127.0.0.1:5001', 'http://127.0.0.1:3000'];
 
@@ -41,12 +40,10 @@ try {
     exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
         cors(corsOptions)(req, res, () => {
             res.status(200).send({
-=======
     // Key Request HTTP function
     exports.getFirebaseConfig = functions.https.onRequest((req, res) => {
         corsHandler(req, res, () => {
             res.send({
->>>>>>> main
                 apiKey: API_KEY,
                 authDomain: AUTH_DOMAIN,
                 projectId: PROJECT_ID,
@@ -56,11 +53,8 @@ try {
         });
     });
 } catch (error) {
-<<<<<<< HEAD
     console.error("Error initializing Firebase config:", error);
 }
-=======
     console.error("Error initializing Firebase configuration:", error.message);
     console.error("Stack trace:", error.stack);
 }
->>>>>>> main
